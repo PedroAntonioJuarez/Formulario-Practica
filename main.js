25daca1281b65b39ba2c8b77faa56a03f5cb46e4
Pedro Juarez <pedrojuarezvisuara@gmail.com>
2021-09-04 17:46:58 -0300

    modificacon de codigo

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const myForm = event => {
     const formState = []
     let name = document.getElementById('name')
     let lastname = document.getElementById('lastname')
+    let edad = document.getElementById('edad')
     let email = document.getElementById('email')
+    let checkbox = document.getElementById('checkbox')
 
 
     !name.value ? name.classList.add('error') :
@@ -12,11 +14,16 @@ const myForm = event => {
     !lastname.value ? lastname.classList.add('error') :
     formState.push({element: 'lastname', value: lastname.value }) && lastname.classList.remove('error')
 
+    !edad.value ? edad.classList.add('error') :
+    formState.push({element: 'edad', value: edad.value }) && edad.classList.remove('error')
+
     !email.value ? email.classList.add('error') :
     formState.push ({element: 'email', value: email.value}) && email.classList.remove('error')
 
-    const validacion = document.getElementById('FormValues')
+    if (document.getElementById('checkbox').checked)
+      {} else {alert ('Acepta terminos y condiciones')}
 
-    formState.length === 3 && confirm ("TUS DATOS SERAN ENVIADOS (TOCA ACEPTAR)") && (validacion.innerHTML = JSON.stringify(formState))
+   
+   formState.length === 4 && confirm ("TUS DATOS SERAN ENVIADOS (TOCA ACEPTAR)")
 }
 
